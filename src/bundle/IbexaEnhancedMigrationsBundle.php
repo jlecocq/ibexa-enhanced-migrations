1026211f7449c76b54959a6deb8efaea6b6de21e
Jérôme Lecocq <jerome.lecocq.freelance@ik.me>
2025-09-01 23:48:28 +0200

    Fix ibexa 5 compatibility.

--- src/bundle/IbexaEnhancedMigrationsBundle.php
+++ src/bundle/IbexaEnhancedMigrationsBundle.php
@@ -3,11 +3,12 @@
 namespace JL\IbexaEnhancedMigrations;
 
 use JL\IbexaEnhancedMigrations\DependencyInjection\IbexaEnhancedMigrationsExtention;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IbexaEnhancedMigrationsBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new IbexaEnhancedMigrationsExtention();
     }
